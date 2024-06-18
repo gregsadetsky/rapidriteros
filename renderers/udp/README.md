885bff19c58f3b3e97d2243ba18fcdf9d83087e8
Antoine Leclair <antoineleclair@gmail.com>
2024-06-18 09:45:25 -0400

    Work on UDP/OSC renderer

---
+++ renderers/udp/README.md
@@ -0,0 +1,15 @@
+# OSC
+
+## Build
+```bash
+docker build -t rapidriteros/udp .
+```
+
+## Run
+```bash
+docker run \
+    --publish published=12000,target=12000,protocol=udp \
+    --publish 8080:80 \
+    --volume .:/app \
+    rapidriteros/udp
+```
---
+++ renderers/udp/osc.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from sse_starlette import ServerSentEvent
+from sse_starlette.sse import EventSourceResponse
+
+from fastapi import FastAPI
+
+logging.basicConfig(level=logging.INFO)
+
+log = logging.getLogger(__name__)
+
+log.info("Initializing OSC")
+
+UDP_PORT = 12000
+
+queues: list[asyncio.Queue[str]] = []
+
+
+class OscUDPServer(asyncio.DatagramProtocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        global queues
+        try:
+            osc_str = data.decode("utf-8")
+            log.info("UDP packet received: %s", osc_str)
+        except UnicodeDecodeError:
+            log.error("Failed to UTF-8 decode OSC str: %s", data)
+            return
+        for queue in queues:
+            queue.put_nowait(osc_str)
+
+    def connection_lost(self, exception):
+        try:
+            self.transport.close()
+        except Exception:
+            pass
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    log.info("Starting UDP server")
+    loop = asyncio.get_running_loop()
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: OscUDPServer(),
+        local_addr=("0.0.0.0", UDP_PORT),
+    )
+    yield
+    try:
+        transport.close()
+        log.info("Closed datagram log endpoint")
+    except Exception:
+        log.exception("Exception closing transport")
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+async def render_stream() -> AsyncGenerator[ServerSentEvent, None]:
+    global queues
+    log.info("New HTTP connection for /render")
+    queue: asyncio.Queue[str] = asyncio.Queue()
+    queues.append(queue)
+    try:
+        while True:
+            osc_str = await queue.get()
+            yield ServerSentEvent(
+                event="screen_update",
+                data=osc_str,
+            )
+    finally:
+        log.info("HTTP connection for /render disconnected")
+        queues.remove(queue)
+
+
+@app.get("/render")
+async def render():
+    return EventSourceResponse(render_stream())
+
+
+log.info("OSC server running")
