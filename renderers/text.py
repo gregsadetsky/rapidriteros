21067d50df952d14987043e176211e274ab4bba9
gregsadetsky <lepetitg@gmail.com>
2024-06-17 22:21:21 -0400

    .

---
+++ renderers/text.py
@@ -0,0 +1,32 @@
+import random
+from time import sleep
+
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+
+@app.route("/render", methods=["POST"])
+def render():
+    print("payload", request.json)
+
+    def eventStream():
+        expo_timeout = 1
+
+        while True:
+            # wait for source data to be available, then push it
+            random_payload = random.random()
+            yield f"event: screen_update\ndata: {random_payload}\n\n"
+
+            sleep(1)
+
+            expo_timeout += 1
+            if expo_timeout > 5:
+                yield "event: end\n\n"
+                break
+
+    return Response(eventStream(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
---
+++ worker.py
@@ -0,0 +1,66 @@
+from itertools import cycle
+from time import sleep
+
+import requests
+import sseclient
+from requests.exceptions import ConnectionError
+
+RENDERER_URLS = {
+    "text": "http://127.0.0.1:5000/render",
+    "udp": "http://127.0.0.1:6000/render",
+}
+CURRENT_RENDERER_NAME = "text"
+
+
+def receive_frames_from_renderer(renderer_name):
+    try:
+        response = requests.post(
+            RENDERER_URLS[renderer_name],
+            headers={"Accept": "text/event-stream"},
+            stream=True,
+            json={"payload": "some data"},
+            # timeout is a tuple of (connect, read) second timeout values
+            timeout=(1, 5),
+        )
+    except requests.RequestException as e:
+        print("request exception!!", e)
+        return
+
+    assert response.status_code == 200
+
+    # if response.status_code != 200:
+    # log.info("Screen endpoint returned %d", response.status_code)
+    #     return
+
+    try:
+        for event in sseclient.SSEClient(response).events():
+            if event.event == "screen_update":
+                yield event.data
+                # base64_text = event.data
+                # image_bytes = base64.b64decode(base64_text)
+                # log.info("Image: %d", image_bytes[0])
+            elif event.event == "end":
+                return
+    except requests.RequestException as e:
+        print("request exception!!", e)
+
+
+def show_frame(frame_data):
+    print("PRINTING TO SCREEN >>>", frame_data)
+
+
+def worker():
+    all_renderers = cycle(["text", "udp"])
+
+    while True:
+        print("Worker is working...")
+
+        for renderer in all_renderers:
+            print("Current renderer:", renderer)
+            for frame in receive_frames_from_renderer(renderer):
+                # we processed 1 frame, we can do other things now
+                show_frame(frame)
+
+
+if __name__ == "__main__":
+    worker()
