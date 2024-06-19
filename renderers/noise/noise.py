42571120d974583d25389db32119130b329add49
gregsadetsky <lepetitg@gmail.com>
2024-06-18 20:37:57 -0400

    .

---
+++ renderers/noise/noise.py
@@ -0,0 +1,40 @@
+import random
+from base64 import b64encode
+from time import sleep
+
+from flask import Flask, Response, request
+from PIL import Image
+
+app = Flask(__name__)
+
+
+@app.route("/render", methods=["POST"])
+def render():
+    def eventStream():
+        frames = 1
+
+        while True:
+            # generate a 96 x 38 black and white 1 channel image
+            # and fill it with random noise
+
+            random_image = Image.new("1", (96, 38))
+            random_image.putdata([random.randint(0, 1) for _ in range(96 * 38)])
+            # get png data
+            random_image_bytes = random_image.tobytes()
+            # base64 encode
+            random_image_base64 = b64encode(random_image_bytes).decode("utf-8")
+
+            yield f"event: screen_update\ndata: {random_image_base64}\n\n"
+
+            sleep(1)
+
+            frames += 1
+            if frames > 30:
+                yield "event: end\n\n"
+                break
+
+    return Response(eventStream(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True, port=8000, host="0.0.0.0")
--- worker/worker.py
+++ worker/worker.py
@@ -1,14 +1,30 @@
+import base64
+import io
+import socket
 from itertools import cycle
 from time import sleep
 
+import numpy as np
 import requests
 import sseclient
+from PIL import Image
 from requests.exceptions import ConnectionError
 
 RENDERER_URLS = {
-    "text": "http://renderertext/render",
-    "udp": "http://rendererosc/render",
+    "noise": "http://localhost:8000/render"
+    # "text": "http://renderertext/render",
+    # "udp": "http://rendererosc/render",
 }
+ALL_RENDERERS = list(RENDERER_URLS.keys())
+
+# PROD i.e. disco on raspi
+SCREEN_UDP_IP = "10.0.0.42"
+SCREEN_UDP_PORT = 6450
+
+SCREEN_WIDTH = 96
+SCREEN_HEIGHT = 38
+
+SCREEN_SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 
 
 def receive_frames_from_renderer(renderer_name):
@@ -34,22 +50,39 @@ def receive_frames_from_renderer(renderer_name):
     try:
         for event in sseclient.SSEClient(response).events():
             if event.event == "screen_update":
-                yield event.data
-                # base64_text = event.data
-                # image_bytes = base64.b64decode(base64_text)
-                # log.info("Image: %d", image_bytes[0])
+                print("event.data", event.data)
+                base64_text = event.data
+                image_bytes = base64.b64decode(base64_text)
+                yield image_bytes
             elif event.event == "end":
                 return
     except requests.RequestException as e:
         print("request exception!!", e)
 
 
-def show_frame(frame_data):
-    print("PRINTING TO SCREEN >>>", frame_data)
+def send_frame_to_display(png_image_data):
+    # expecting base64 1 channel png, fail otherwise
+    pil_image = Image.frombytes("1", (96, 38), png_image_data)
+    # assert that it's exactly 96x38
+    assert pil_image.size == (SCREEN_WIDTH, SCREEN_HEIGHT)
+    # assert that its mode is 1 i.e. black and white
+    assert pil_image.mode == "1"
+
+    frame = np.zeros((SCREEN_HEIGHT * SCREEN_WIDTH, 1), bool)
+
+    np_image = np.asarray(pil_image)
+
+    # flip img from right-left to left-right
+    np_image = np.flip(np_image, 1)
+
+    frame_packed_bits = np.packbits(
+        np_image.astype("bool"), axis=None, bitorder="little"
+    )
+    SCREEN_SOCK.sendto(frame_packed_bits, (SCREEN_UDP_IP, SCREEN_UDP_PORT))
 
 
 def worker():
-    all_renderers = cycle(["udp", "text"])
+    all_renderers = cycle(ALL_RENDERERS)
 
     while True:
         print("Worker is working...")
@@ -58,7 +91,7 @@ def worker():
             print("Current renderer:", renderer)
             for frame in receive_frames_from_renderer(renderer):
                 # we processed 1 frame, we can do other things now
-                show_frame(frame)
+                send_frame_to_display(frame)
             sleep(1)
 
 
