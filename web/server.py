defae210484b50643ea3d9c3febe70425d4b736a
gregsadetsky <lepetitg@gmail.com>
2024-06-21 14:59:21 -0400

    .

--- web/server.py
+++ web/server.py
@@ -1,14 +1,36 @@
+import argparse
+import math
+import random
+import time
 from datetime import datetime
 
 from flask import Flask
+from pythonosc import udp_client
 
 app = Flask(__name__)
+
+LOCAL_OSC_SERVER_IP = "127.0.0.1"  # ???
+LOCAL_OSC_SERVER_PORT = 12000
 
 
 @app.route("/")
 def hello_world():
     print("new web request")
     return f"hello from disco!!! the datetime is {datetime.now()}"
+
+
+@app.route("/send-one-frame-by-osc")
+def send_one_frame():
+    client = udp_client.SimpleUDPClient(LOCAL_OSC_SERVER_IP, LOCAL_OSC_SERVER_PORT)
+
+    out = ""
+    for y in range(38):
+        for x in range(96):
+            # (x ^ y) % 9
+            out += "1" if ((x + x_delta) ^ (y + y_delta)) % 9 == 0 else "0"
+    client.send_message("/screen", out)
+
+    print("sent")
 
 
 if __name__ == "__main__":
