a118fbd92c0983a6a14798559352acc5e3063540
gregsadetsky <lepetitg@gmail.com>
2024-06-21 20:22:19 -0400

    .

--- worker/worker.py
+++ worker/worker.py
@@ -104,7 +104,7 @@ def worker():
             if renderer == "text":
                 json_payload = {
                     # "text": "Send 3648 chars of 0 and 1 by OSC to 10.100.7.28 port 12000. the OSC path does not matter. xx",
-                    "text": "ALGORAVE! D A N C E ALGORAVE! D A N C E ALGORAVE! D A N C E ALGORAVE! D A N C E",
+                    "text": "ALGORAVE! D A N C E  ALGORAVE! D A N C E  ALGORAVE! D A N C E  ALGORAVE! D A N C E",
                 }
             for frame in receive_frames_from_renderer(
                 renderer, json_payload=json_payload
