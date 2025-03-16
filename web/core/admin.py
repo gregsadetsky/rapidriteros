d5270540f82a6fd0ebdb732fb9833dcdf1f06c07
Greg Sadetsky <lepetitg@gmail.com>
2025-03-16 00:04:43 -0400

    Update admin.py

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
-
 from .models import KV, Show
-
 
 class ShowAdmin(admin.ModelAdmin):
     list_display = ("created_at", "show_type", "payload", "disabled")
-
+    actions = ['bulk_disable']
+    
+    def bulk_disable(self, request, queryset):
+        updated = queryset.update(disabled=True)
+        if updated == 1:
+            message_bit = "1 show was"
+        else:
+            message_bit = f"{updated} shows were"
+        self.message_user(request, f"{message_bit} successfully disabled.")
+    bulk_disable.short_description = "Disable selected shows"
 
 admin.site.register(Show, ShowAdmin)
 
-
 class KVAdmin(admin.ModelAdmin):
     list_display = ("key", "value")
-
 
 admin.site.register(KV, KVAdmin)
