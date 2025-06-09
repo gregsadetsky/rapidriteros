0f48157abd505509100799187a4313e248726059
Greg Sadetsky <lepetitg@gmail.com>
2025-06-09 12:26:18 -0400

    Update offline-canvas-p5.js

--- renderers/p5/subrenderer/offline-canvas-p5.js
+++ renderers/p5/subrenderer/offline-canvas-p5.js
@@ -23,7 +23,7 @@ global.Event = global.window.Event;
 global.Image = global.window.Image;
 
 const startTime = Date.now();
-const RUN_PROGRAM_FOR_SECONDS = 300;
+const RUN_PROGRAM_FOR_SECONDS = 30;
 
 // this determines how long our program runs!!!
 global.window.requestAnimationFrame = (callback) => {
